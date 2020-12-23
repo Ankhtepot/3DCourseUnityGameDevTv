2319e17db0a1d9fe29412dec9fcf91811a59f732
PetrZavodny <petr.zavodny@chyronhego.com>
2020-12-23 20:38:07 +0100

    Up to lesson 214

---
+++ ZombieRunner/Assets/Scripts/BaterryPickup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+//Fireball Games * * * PetrZavodny.com
+
+public class BaterryPickup : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] private float angleRestore = 20f;
+    [SerializeField] private float intensityRestore = 5f;
+
+    private FlashlightSystem flashlight;
+#pragma warning restore 649
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            flashlight = other.GetComponentInChildren<FlashlightSystem>();
+            Debug.Log($"BaterryPickup triggered");
+            flashlight.RestoreLightAngle(angleRestore);
+            flashlight.RestoreLightIntensity(intensityRestore);    
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ ZombieRunner/Assets/Scripts/FlashlightSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Fireball Games * * * PetrZavodny.com
+
+public class FlashlightSystem : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] private float lightDecay = 0.1f;
+    [SerializeField] private float angleDecay = 1f;
+    [SerializeField] private float minimumAngle = 30f;
+    [SerializeField] private Light myLight;
+#pragma warning restore 649
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle += restoreAngle;
+    }
+
+    public void RestoreLightIntensity(float intensityAmount)
+    {
+        myLight.intensity += intensityAmount;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle > minimumAngle)
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+}
